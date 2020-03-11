4fa3d0c91df1b0bbd6d71d54a795d5f4cc748250
SimonJNicol <simon.j.nicolsemail@gmail.com>
2020-03-11 02:02:55 -0700

    Add files via upload

---
+++ KeystonePlugin/WindowsFormsKeystonePlug/WindowsFormsKeystonePlug/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace KeystonePlugin
+
+{
+    partial class BeamDesigner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Select Structural",
+            "No.1 & Btr",
+            "No.1",
+            "No.1/ No.2",
+            "No.2",
+            "No.3",
+            "Stud",
+            "Construction",
+            "Standard",
+            "Utility",
+            "All Above"});
+            this.checkedListBox1.Location = new System.Drawing.Point(490, 48);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(274, 191);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "All",
+            "All MSR Lumber (Machine Stress Rated)",
+            "Douglas Fir-Larch",
+            "Douglas Fir-Larch (North)",
+            "Douglas Fir-South",
+            "Alaska Cedar",
+            "Alaska Hemlock",
+            "Alaska Spruce",
+            "Alaska Yellow Cedar",
+            "Aspen",
+            "Baldcypress",
+            "Beech-Birch-Hickory",
+            "Coast Sitka Spruce",
+            "Cottonwood",
+            "Eastern Hemlock-Balsam Fir",
+            "Eastern Hemlock-Tamarack",
+            "Eastern White Pine",
+            "Hem-Fir",
+            "Hem-Fir (North)",
+            "Mixed Maple",
+            "Mixed Oak",
+            "Northern Red Oak",
+            "Northern Species",
+            "Northern White Cedar",
+            "Red Maple"});
+            this.checkedListBox2.Location = new System.Drawing.Point(46, 264);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(274, 140);
+            this.checkedListBox2.TabIndex = 1;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Dimensional Lumber",
+            "Timbers",
+            "Beams and Stringers",
+            "Glulams"});
+            this.comboBox1.Location = new System.Drawing.Point(84, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(46, 189);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(189, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Show only minimum sizes";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 381);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Beam Designer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(664, 264);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(520, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Length of Beam (ft):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(495, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Select Wood Quality:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Select Wood Species:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Select Beam Type:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(551, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Live Load (plf):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(664, 292);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(543, 323);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Dead Load (plf):";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(664, 320);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.BeamDesigner_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ KeystonePlugin/WindowsFormsKeystonePlug/WindowsFormsKeystonePlug/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeystonePlugin
+{
+    public partial class BeamDesigner : Form
+    {
+        public BeamDesigner()
+        {
+            InitializeComponent();
+        }
+
+        private void BeamDesigner_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            StringBuilder woodQuality = new StringBuilder("", 11);
+            StringBuilder woodSpecies = new StringBuilder("", 25);
+            string cutType = string.Empty;
+            string output = string.Empty;
+            char minResults = '0';
+            double beamLength = -1;
+            double liveLoad = -1;
+            double deadLoad = -1;
+
+            string temp = textBox1.Text;
+
+            if (temp.Equals(""))
+            {
+                MessageBox.Show("Beam Length Forgotten... Cannot Calculate... Aborting...");
+                System.Windows.Forms.Application.Exit();
+            }
+
+            else
+            {
+                beamLength = double.Parse(temp);
+            }
+
+            temp = textBox2.Text;
+
+            if (temp.Equals(""))
+            {
+                MessageBox.Show("Live Load Forgotten... Cannot Calculate... Aborting...");
+                System.Windows.Forms.Application.Exit();
+            }
+
+            else
+            {
+                liveLoad = double.Parse(temp);
+            }
+
+            temp = textBox3.Text;
+
+            if (temp.Equals(""))
+            {
+                MessageBox.Show("Beam Length Forgotten... Cannot Calculate... Aborting...");
+                System.Windows.Forms.Application.Exit();
+            }
+
+            else
+            {
+                deadLoad = double.Parse(temp);
+            }
+
+            if (checkBox1.Checked)
+            {
+                minResults = '1';
+            }
+
+            temp = comboBox1.SelectedItem.ToString();
+            
+            switch (temp)
+            {
+                case "Dimensional Lumber":
+                    cutType = "1000";
+                    break;
+                case "Timbers":
+                    cutType = "0100";
+                    break;
+                case "Beams and Stringers":
+                    cutType = "0010";
+                    break;
+                case "Glulams":
+                    cutType = "0001";
+                    break;
+
+                default:
+                    MessageBox.Show("#ERROR SELECTING CUT TYPE#\n#ABORTING...");
+                    cutType = "0000";
+                    break;
+            }
+            
+            while (i < 11)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    woodQuality.Append("1");
+                }
+                else
+                {
+                    woodQuality.Append("0");
+                }
+                i++;
+            }
+            
+//            MessageBox.Show("" + i);
+            i = 0;
+            
+            while (i < 25)
+            {
+                if (checkedListBox2.GetItemChecked(i))
+                {
+                    woodSpecies.Append("1");
+                }
+                else
+                {
+                    woodSpecies.Append("0");
+                }
+                i++;
+            }
+            //            MessageBox.Show("" + i);
+            output = woodQuality + "," + woodSpecies + "," + cutType + "," + minResults + "," 
+                + beamLength + "," + liveLoad + "," + deadLoad;
+            System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\" + "output.txt", output);
+            MessageBox.Show("Complete.");
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
